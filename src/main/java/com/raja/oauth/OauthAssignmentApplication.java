ab2585aea21688205612bbf175ba442d64982be1
Raja-r-art <raja.r2024aiml@sece.ac.in>
2026-06-09 10:19:09 +0530

    oauth assignment completed

---
+++ src/main/java/com/raja/oauth/OauthAssignmentApplication.java
@@ -0,0 +1,12 @@
+package com.raja.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OauthAssignmentApplication.class, args);
+    }
+}
---
+++ src/test/java/com/raja/oauth/OauthAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.raja.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthAssignmentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
